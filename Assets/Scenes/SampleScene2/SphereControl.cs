6dc2522ce2527dd2b333b753464161a53ebb262a
MinPan9585 <panmin677095@gmail.com>
2025-10-01 22:07:09 +0800

    add canControl variable, diable it after 3 sec

--- Assets/Scenes/SampleScene2/SphereControl.cs
+++ Assets/Scenes/SampleScene2/SphereControl.cs
@@ -28,6 +28,10 @@ public class SphereControl : MonoBehaviour
     private Vector3 initialPosition;
     private Vector3 initialMousePosition;
 
+    // 新增变量
+    public bool canControl = true; // 是否允许鼠标控制
+    private Coroutine countdownCoroutine;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -40,17 +44,22 @@ public class SphereControl : MonoBehaviour
         // 根据设置的按键类型来检测鼠标输入
         int mouseButtonIndex = (controlButton == MouseButton.LeftButton) ? 0 : 1;
 
-        //如果设置的鼠标键按下
-        if (Input.GetMouseButtonDown(mouseButtonIndex))
+        // 鼠标按下且允许控制时，启动倒计时
+        if (Input.GetMouseButtonDown(mouseButtonIndex) && canControl)
         {
             isHolding = true;
             // 记录开始holding时的物体位置和鼠标位置
             initialPosition = transform.position;
             initialMousePosition = Input.mousePosition;
             rb.drag = 22f;
+
+            // 启动倒计时协程
+            if (countdownCoroutine != null)
+                StopCoroutine(countdownCoroutine);
+            countdownCoroutine = StartCoroutine(HoldCountdown());
         }
-        //如果设置的鼠标键松开
-        if (Input.GetMouseButtonUp(mouseButtonIndex))
+
+        if (Input.GetMouseButtonUp(mouseButtonIndex) && isHolding)
         {
             rb.drag = 0f;
             StartCoroutine(TemporaryKinematic());
@@ -66,7 +75,7 @@ public class SphereControl : MonoBehaviour
     void FixedUpdate()
     {
 
-        if (isHolding)
+        if (isHolding && canControl)
         {
             rb.useGravity = false;
             // 计算鼠标移动的偏移量
@@ -95,6 +104,23 @@ public class SphereControl : MonoBehaviour
             rb.useGravity = true;
             // add a force to the sphere
             rb.AddForce(Vector3.forward * force);
+        }
+    }
+
+    // 倒计时协程
+    private IEnumerator HoldCountdown()
+    {
+        yield return new WaitForSeconds(2f);
+        canControl = false;
+        //isHolding = false;
+    }
+
+    // 恢复鼠标控制
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Grabable"))
+        {
+            canControl = true;
         }
     }
 
---
+++ Assets/Scripts/CanControlImage.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanControlImage : MonoBehaviour
+{
+    public SphereControl sc;
+
+    private void Update()
+    {
+        if(sc.canControl == false)
+        {
+            this.GetComponent<Image>().color = Color.red;
+        }
+        else
+        {
+            this.GetComponent<Image>().color = Color.green;
+        }
+    }
+}
